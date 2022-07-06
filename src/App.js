56f930d0aa2279dafb6f297707f474622c5162f6
Burchin Ismail <burcinismail8@gmail.com>
2022-07-06 09:50:57 +0300

    Navigation finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,33 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <nav className="md:flex flex-row justify-around items-center h-20">
+        <div className="logo ">
+          <img src="/images/logo.svg" alt="logo" />
+        </div>
+        <div className="menu w-1/4">
+          <ul className="md:flex justify-around">
+            <li className="text-gray hover:text-dark-blue hover:underline hover:decoration-lime-green">
+              <a href="/">Home</a>
+            </li>
+            <li className="text-gray hover:text-dark-blue hover:underline hover:decoration-lime-green">
+              <a href="/">About</a>
+            </li>
+            <li className="text-gray hover:text-dark-blue hover:underline hover:decoration-lime-green">
+              <a href="/">Contact</a>
+            </li>
+            <li className="text-gray hover:text-dark-blue hover:underline hover:decoration-lime-green">
+              <a href="/">Blog</a>
+            </li>
+            <li className="text-gray hover:text-dark-blue hover:underline hover:decoration-lime-green">
+              <a href="/">Careers</a>
+            </li>
+          </ul>
+        </div>
+        <div className="registerButton bg-gradient-to-r from-lime-green to-bright-cyan hover:from-light-lime-green hover:to-light-bright-cyan cursor-pointer text-very-light-gray px-8 py-3 rounded-full font-semibold">
+          <a href="/">Request Invite</a>
+        </div>
+      </nav>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      gray: "#A2A4A8",
+      "dark-blue": "hsl(233, 26%, 24%)",
+      "lime-green": "hsl(136, 65%, 51%)",
+      "light-lime-green": "#84E1A9",
+      "bright-cyan": "hsl(192, 70%, 51%)",
+      "light-bright-cyan": "#81D5E0",
+      "grayish-blue": "hsl(233, 8%, 62%)",
+      "light-grayish-blue": "hsl(220, 16%, 96%)",
+      "very-light-gray": " hsl(0, 0%, 98%)",
+    },
   },
   plugins: [],
-}
+};
